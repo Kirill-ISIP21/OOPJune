3288b8edffd49728f1f9aac33cb15230bdd201b2
Kirill-ISIP21 <67316619+Kirill-ISIP21@users.noreply.github.com>
2020-06-24 12:19:52 +0300

    Update Program.cs

--- ООП/ConsoleApp11/ConsoleApp11/Program.cs
+++ ООП/ConsoleApp11/ConsoleApp11/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp11
     {
         static void Main(string[] args)
         {
-            Tri trone = new Tri(8, 8, 8);
+            Tri trone = new Tri(8, 8, 2);
             trone.Display();
-            Tri trtwo = new Tri(7, 9, 5);
+            Tri trtwo = new Tri(7, 9, 55);
             trtwo.Display();
-            Tri tr3 = new Tri(30, 5, 14);
+            Tri tr3 = new Tri(30, 25, 25);
             tr3.Display();
             Console.ReadKey();
         }
@@ -22,20 +22,20 @@ namespace ConsoleApp11
     class Tri
     {
         private bool Trig;
-        private double A;
-        private double B;
-        private double G;
+        private double a;
+        private double b;
+        private double c;
         public Tri(int l1, int l2, int l3)
         {
             double A = (Math.Acos((Math.Pow(l2, 2) + Math.Pow(l3, 2) - Math.Pow(l3, 2)) / (2 * l1 * l2))) * (180 / Math.PI);
             double B = (Math.Acos((Math.Pow(l1, 2) + Math.Pow(l2, 2) - Math.Pow(l1, 2)) / (2 * l2 * l3))) * (180 / Math.PI);
-            double G = (Math.Acos((Math.Pow(l3, 2) + Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * l3))) * (180 / Math.PI);
+            double C = (Math.Acos((Math.Pow(l3, 2) + Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * l3))) * (180 / Math.PI);
             if ((l1 + l2 > l3) && (l2 + l3 > l1) && (l1 + l3 > l2))
             {
                 Trig = true;
-                A = A;
-                B = B;
-                G = G;
+                a = A;
+                b = B;
+                c = C;
             }
             else
             {
@@ -46,10 +46,10 @@ namespace ConsoleApp11
         {
             if (Trig == true)
             {
-                Console.WriteLine($"\nКоординаты треугольника \n\n" +
-                                  $"A : {A.ToString("##.#")}" +
-                                  $"\nB : {B.ToString("##.#")}" +
-                                  $"\nG: {G.ToString("##.#")}");
+                Console.WriteLine($"\nУглы треугольника \n\n" +
+                                  $"A: {a.ToString("##.#")}" +
+                                  $"\nB: {b.ToString("##.#")}" +
+                                  $"\nC: {c.ToString("##.#")}");
             }
             else
                 Console.WriteLine("\nТреугольник не построить");
